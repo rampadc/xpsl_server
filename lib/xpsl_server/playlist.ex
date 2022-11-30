fa0583c36d58dd6331d69ed04c73b0c591918414
Cong Nguyen <cong.nguyen@au1.ibm.com>
2022-11-30 20:54:44 +1100

    renamed functions and added next prev commands

--- lib/xpsl_server/playlist.ex
+++ lib/xpsl_server/playlist.ex
@@ -1,18 +1,39 @@
 defmodule Playlist do
-  @enforce_keys [:tracks]
-  defstruct tracks: nil
+  @enforce_keys [:tracks, :previous_tracks]
+  defstruct tracks: nil, previous_tracks: nil
 
   @doc """
   Creates a new playlist with an empty list of tracks
   """
   def new() do
-    %Playlist{tracks: []}
+    %Playlist{tracks: [], previous_tracks: []}
   end
 
   @doc """
   Add a track to the playlist
   """
-  def add_track(playlist, track) do
+  def queue(playlist, track) do
     %{playlist | tracks: [track | playlist.tracks]}
+  end
+
+  @doc """
+  Next track command moves the current track to the top of the `previous_tracks` list
+  """
+  def next(playlist) do
+    {current_track, future_tracks} = List.pop_at(playlist.tracks, 0)
+    %{playlist | tracks: future_tracks, previous_tracks: [current_track | playlist.previous_tracks]}
+  end
+
+  @doc """
+  Previous track command moves the top `previous_tracks` track to the head of the currently playing `tracks` list
+  """
+  def previous(playlist) do
+    {to_be_current_track, other_previous_tracks} = List.pop_at(playlist.previous_tracks, 0)
+
+    if to_be_current_track do
+      %{playlist | tracks: [to_be_current_track | playlist.tracks], previous_tracks: other_previous_tracks}
+    else
+      playlist
+    end
   end
 end
--- test/playlist_test.exs
+++ test/playlist_test.exs
@@ -7,20 +7,64 @@ defmodule PlaylistTest do
     playlist = Playlist.new()
 
     assert playlist.tracks == []
+    assert playlist.previous_tracks == []
   end
 
-  test "add new tracks to a playlist" do
+  test "queue a track" do
     playlist = Playlist.new()
 
     track1 = Track.new("songwhip_url1", 0)
     track2 = Track.new("songwhip_url2", 2)
 
-    playlist = Playlist.add_track(playlist, track1)
-    playlist = Playlist.add_track(playlist, track2)
+    playlist = Playlist.queue(playlist, track1)
+    playlist = Playlist.queue(playlist, track2)
 
     assert Map.equal?(List.first(playlist.tracks), track2)
 
     [_current_track | prev_tracks] = playlist.tracks
     assert Map.equal?(List.first(prev_tracks), track1)
+  end
+
+  test "get the next track" do
+    playlist = Playlist.new()
+
+    track1 = Track.new("songwhip_url1", 0)
+    track2 = Track.new("songwhip_url2", 2)
+
+    playlist = playlist |> Playlist.queue(track1) |> Playlist.queue(track2) |> Playlist.next()
+
+    assert Map.equal?(List.first(playlist.tracks), track1)
+    assert Map.equal?(List.first(playlist.previous_tracks), track2)
+  end
+
+  test "previous tracks should return the same playlist if there are no tracks in previous_tracks list" do
+    playlist = Playlist.new()
+
+    track1 = Track.new("songwhip_url1", 0)
+    track2 = Track.new("songwhip_url2", 2)
+
+    playlist = playlist
+               |> Playlist.queue(track1)
+               |> Playlist.queue(track2)
+    new_playlist = Playlist.previous(playlist)
+
+    assert new_playlist.previous_tracks == []
+    assert new_playlist.tracks == playlist.tracks
+  end
+
+  test "previous track command should move the head of `previous_tracks` list to the head of `tracks` list" do
+    playlist = Playlist.new()
+
+    track1 = Track.new("songwhip_url1", 0)
+    track2 = Track.new("songwhip_url2", 2)
+
+    playlist = playlist
+               |> Playlist.queue(track1)  # tracks: [t1]
+               |> Playlist.queue(track2)  # tracks: [t2, t1]
+               |> Playlist.next()         # tracks: [t1], previous_tracks: [t2]
+               |> Playlist.previous()  # tracks: [t2, t1], previous_tracks: []
+
+    assert Map.equal?(List.first(playlist.tracks), track2)
+    assert playlist.previous_tracks == []
   end
 end
